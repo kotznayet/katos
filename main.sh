44659219a087a3f8597d23caa056d332c0ae2df3
kotznayet <k3553620@gmail.com>
2026-01-15 18:37:40 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install plasma-desktop sddm plasma-session-wayland upower udisks2 plasma-nm kscreen partitionmanager -y --no-install-recommends
+sudo apt install plasma-desktop sddm plasma-workspace kwin-wayland upower udisks2 plasma-nm kscreen partitionmanager -y --no-install-recommends
 sudo apt install gldriver-test dolphin konsole kdialog kate vlc kde-config-sddm bluedevil plasma-browser-integration wget -y 
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 sudo systemctl enable sddm.service 
