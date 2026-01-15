c52bd593028d3d052cc843acec906ed219843c62
kotznayet <k3553620@gmail.com>
2026-01-15 18:21:40 +0300

    Updated maybe this one will work

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install plasma-desktop plasma-workspace sddm kwin-wayland -y --no-install-recommends
-sudo apt install gldriver-test dolphin konsole kdialog vlc kde-config-sddm curl -y 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/kotznayet/katos/master/tools/quiet-piapps.sh)"
+sudo apt install plasma-desktop sddm plasma-session-wayland upower udisks2 plasma-nm kscreen partitionmanager -y --no-install-recommends
+sudo apt install gldriver-test dolphin konsole kdialog kate vlc kde-config-sddm bluedevil plasma-browser-integration wget -y 
+wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 sudo systemctl enable sddm.service 
+sudo systemctl set-default graphical.target
 sudo systemctl start sddm.service
