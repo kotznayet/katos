e23a73b64d50dfa80ecaa3620d96afefc1e65e3a
kotznayet <k3553620@gmail.com>
2026-01-15 18:40:43 +0300

    no recomendations at ALL

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install plasma-desktop sddm plasma-workspace kwin-wayland upower udisks2 plasma-nm kscreen partitionmanager -y --no-install-recommends
-sudo apt install gldriver-test dolphin konsole kdialog kate vlc kde-config-sddm bluedevil plasma-browser-integration wget -y 
+sudo apt install gldriver-test dolphin konsole kdialog kate vlc kde-config-sddm bluedevil plasma-browser-integration wget -y --no-install-recommends
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 sudo systemctl enable sddm.service 
 sudo systemctl set-default graphical.target
