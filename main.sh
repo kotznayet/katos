eb39ca6390eb705a3c805e51f445c282baac436d
kotznayet <k3553620@gmail.com>
2026-01-23 17:54:02 +0300

    Fix

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install plasma-desktop sddm plasma-workspace kwin-wayland upower udisks2 plasma-nm kscreen partitionmanager chromium -y --no-install-recommends
-sudo apt install gldriver-test dolphin konsole kdialog kate vlc kde-config-sddm bluedevil plasma-browser-integration wget -y --no-install-recommends
-yes | sh -c "$(wget https://raw.githubusercontent.com/Botspot/pi-apps/master/install -O -)"
+sudo apt install gldriver-test dolphin konsole kdialog kate vlc kde-config-sddm bluedevil plasma-browser-integration wget git -y --no-install-recommends
+wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 yes | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-touch /etc/cloud/cloud-init.disable
+sudo touch /etc/cloud/cloud-init.disable
 sudo systemctl enable sddm.service 
 sudo systemctl set-default graphical.target
 sudo systemctl start sddm.service
