fc5c8e80cb197cec6be8c4d650cf5869d8ca794c
kotznayet <k3553620@gmail.com>
2026-02-16 19:05:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-screen -S plasma_install bash -c 'bash <(curl -fsSL https://raw.githubusercontent.com/kotznayet/katos/main/main.sh)'
+sudo apt install -y screen && screen -S katos_install bash -c 'bash <(curl -fsSL https://raw.githubusercontent.com/kotznayet/katos/main/main.sh)'
